c20553a5cb38892e41eb146b138eb8bc81b31f02
Priyadharshini Murugan <priyadharshini.mu@itp.objectfrontier.com>
2023-09-13 09:30:52 +0530

    first release

--- gitpractice/Program.cs
+++ gitpractice/Program.cs
@@ -16,6 +16,7 @@ class Pig : Animal
   {
     // The body of animalSound() is provided here
     Console.WriteLine("The pig says: wee wee");
+    Console.WriteLine("The dog says bow bow");
   }
 }
 
